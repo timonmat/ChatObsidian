3cc32984ce30ccaa511de0eca56369ada14bd88f
timonmat <matti@gbuilder.com>
2023-03-20 12:37:53 +0200

    spinner on query, restructure gptsimple usage a bit.

--- pages/Query.py
+++ pages/Query.py
@@ -1,13 +1,10 @@
-#Query.py
 import streamlit as st
-from llama_index import GPTSimpleVectorIndex
 from llama_index.response.schema import SourceNode
 from pathlib import Path
+from contextlib import suppress
 
 from components.sidebar import add_to_sidebar
-from utils.qa_template import QA_PROMPT
-from utils.model_settings import get_embed_model, get_llm_predictor, get_prompt_helper
-from utils.GPTSimple import load_index
+from utils.GPTSimple import  query_gptsimpleindex
 
 st.set_page_config(
     page_title="Query",
@@ -15,10 +12,6 @@ st.set_page_config(
 )
 
 add_to_sidebar()
-
-embed_model = get_embed_model()
-llm_predictor = get_llm_predictor()
-prompt_helper = get_prompt_helper()
 
 st.write("# ChatObsidian 🔍  \n")
 st.write("### Query your data from GPTSimpleIndex")
@@ -28,42 +21,25 @@ def clear_submit():
 
 clear_submit()
 
-
-if st.session_state.get("api_key_configured"):
-    if Path('index.json').exists():
-        index = load_index()
-        st.sidebar.success("Index loaded")
-    else:
-        st.sidebar.error("Index not found")
-        
-          
 query_str = st.text_area("Ask a question about your Markdown notes", on_change=clear_submit)
 
 button = st.button("Submit")
 if button or st.session_state.get("submit"):
     if not st.session_state.get("api_key_configured"):
         st.error("Please configure your OpenAI API key!")
-    elif not index:
-        st.error("Please upload a document!")
     elif not query_str:
         st.error("Please enter a question!")
     else:
         st.session_state["submit"] = True
-        response = index.query(query_str, 
-                               response_mode="compact", # default, compact, tree_summarize 
-                               llm_predictor=llm_predictor, 
-                               prompt_helper=prompt_helper, 
-                               embed_model=embed_model, 
-                               mode="embedding", 
-                               similarity_top_k=5, 
-                               text_qa_template=QA_PROMPT, 
-                               verbose=True)
-        st.markdown(response)
-        
-        with st.expander("Sources"):
-            for node in response.source_nodes:
-                st.write(f"Document ID: {node.doc_id}")
-                st.write(f"Source Text: {node.source_text.strip()}")
-                st.write(f"Similarity: {node.similarity}")
-                st.markdown("---")
-
+        try:
+            with st.spinner("Processing your query..."):
+                response = query_gptsimpleindex(query_str)
+            st.markdown(response)
+            with st.expander("Sources"):
+                for node in response.source_nodes:
+                    st.write(f"Document ID: {node.doc_id}")
+                    st.write(f"Source Text: {node.source_text.strip()}")
+                    st.write(f"Similarity: {node.similarity}")
+                    st.markdown("---")
+        except Exception as e:
+            st.error(f"Error processing your query: {e}")
--- utils/GPTSimple.py
+++ utils/GPTSimple.py
@@ -9,6 +9,31 @@ embed_model = get_embed_model()
 llm_predictor = get_llm_predictor()
 prompt_helper = get_prompt_helper()
 
+index_path = 'index.json'
+index = None
+
 @st.cache_resource
-def load_index():
-    return GPTSimpleVectorIndex.load_from_disk('index.json',embed_model=embed_model, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+def load_gptsimpleindex():
+    if st.session_state.get("api_key_configured"):
+        try:
+            with st.spinner("Loading index..."):
+                index = GPTSimpleVectorIndex.load_from_disk(index_path, embed_model=embed_model, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+            st.sidebar.success("Index loaded")
+            return index
+        except Exception as e:
+            st.exception(f"Error loading index: {e}")
+    else:
+        st.sidebar.error("Index not found")
+    
+
+def query_gptsimpleindex(query_str):
+    index = load_gptsimpleindex()
+    return index.query(query_str,
+                        response_mode="compact", # default, compact, tree_summarize
+                        llm_predictor=llm_predictor,
+                        prompt_helper=prompt_helper,
+                        embed_model=embed_model,
+                        mode="embedding",
+                        similarity_top_k=5,
+                        text_qa_template=QA_PROMPT,
+                        verbose=True)
